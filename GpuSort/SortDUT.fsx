833e8076be503d97a17d97d4e9a696513c6c742c
fierval <fierval@gmail.com>
2016-06-09 10:58:38 -0700

    Tiny tweak

--- GpuSort/SortDUT.fsx
+++ GpuSort/SortDUT.fsx
@@ -13,8 +13,7 @@ let genNonNeg = Arb.generate<int> |> Gen.filter ((<=) 0)
 type Marker =
     static member arbNonNeg = genNonNeg |> Arb.fromGen
     static member ``Sorting Correctly`` arr =
-        let carr = sort arr
-        carr = Array.sort arr
+        sort arr = Array.sort arr
 
 Arb.registerByType(typeof<Marker>)
 Check.QuickAll(typeof<Marker>)
