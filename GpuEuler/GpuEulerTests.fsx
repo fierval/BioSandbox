2f95ddebdeedefa52e51dace6c113745f5cc997d
fierval <fierval@gmail.com>
2016-06-20 22:55:38 -0700

    Sorting & sorting tests

--- GpuEuler/GpuEulerTests.fsx
+++ GpuEuler/GpuEulerTests.fsx
@@ -11,8 +11,10 @@ open FsCheck
 Alea.CUDA.Settings.Instance.Resource.AssemblyPath <- Path.Combine(__SOURCE_DIRECTORY__, @"..\packages\Alea.Cuda.2.2.0.3307\private")
 Alea.CUDA.Settings.Instance.Resource.Path <- Path.Combine(__SOURCE_DIRECTORY__, @"..\release")
 
-type Marker =
-    static member digr = graphGen 3 500 |> Arb.fromGen
+let gr = StrGraph.GenerateEulerGraph(20, 3)
+
+type EulerCycle =
+    static member digr = graphGen 3 100 |> Arb.fromGen
     static member ``Edge Representation on GPU`` (gr : DirectedGraph<string>) =
         let start, end' = gr.Edges
 
@@ -20,6 +22,22 @@ type Marker =
         let startGpu, endGpu = dStart.Gather(), dEnd.Gather()
         start = startGpu && end' = endGpu
 
-Arb.registerByType(typeof<Marker>)
-Check.VerboseAll(typeof<Marker>)
+    static member ``Sort ordinal end edges on GPU`` (gr : DirectedGraph<string>) =
+        let startCpu, endCpu =
+            gr.Edges
+            ||> Array.zip
+            |> Array.sortBy (fun (x, y) -> y, x)
+            |> Array.unzip
+
+        let startGpu, endGpu =
+            let dStart, dEnd = getEdgesGpu gr
+            sortStartEnd dStart dEnd gr.NumEdges
+
+        startCpu = startGpu && endCpu = endGpu
+
+Arb.registerByType(typeof<EulerCycle>)
+Check.QuickAll(typeof<EulerCycle>)
+
+let digr = graphGen 3 100
+let graphs = digr.Sample(3, 100)
 
--- GpuEuler/successor.fs
+++ GpuEuler/successor.fs
@@ -2,12 +2,26 @@
 
 open Graphs
 open Alea.CUDA
+open Alea.CUDA.Unbound
 open Alea.CUDA.Utilities
 open GpuSimpleSort
 
 let blockSize = DirectedGraph.blockSize
 let worker = DirectedGraph.worker
 let target = DirectedGraph.target
+
+[<Kernel; ReflectedDefinition>]    
+let scatter (start : deviceptr<int>) (end' : deviceptr<int>) (len: int) (falsesScan : deviceptr<int>) (revBits : deviceptr<int>) (outStart : deviceptr<int>) (outEnd : deviceptr<int>) =
+    let idx = blockIdx.x * blockDim.x + threadIdx.x
+    if idx < len then
+
+        let totalFalses = falsesScan.[len - 1] + revBits.[len - 1]
+
+        // when the bit is equal to 1 - it will be offset by the scan value + totalFalses
+        // if it's equal to 0 - just the scan value contains the right address
+        let addr = if revBits.[idx] = 1 then falsesScan.[idx] else totalFalses + idx - falsesScan.[idx]
+        outStart.[addr] <- start.[idx]
+        outEnd.[addr] <- end'.[idx]
 
 let getEdgesGpu (gr : DirectedGraph<'a>) =
     let rowIndex = gr.RowIndex
@@ -24,3 +38,38 @@ let getEdgesGpu (gr : DirectedGraph<'a>) =
     worker.Launch <@ DirectedGraph.toEdgesKernel @> lp dRowIndex.Ptr len dColIndex.Ptr dStart.Ptr dEnd.Ptr
 
     dStart, dEnd
+
+let sortStartEnd (dStart : DeviceMemory<int>) (dEnd : DeviceMemory<int>) len =
+    let lp = LaunchParam(divup len blockSize, blockSize)
+
+    use reduceModule = new DeviceReduceModule<int>(target, <@ max @>)
+    use reducer = reduceModule.Create(len)
+
+    use scanModule = new DeviceScanModule<int>(target, <@ (+) @>)
+    use scanner = scanModule.Create(len)
+
+    use dBits = worker.Malloc(len)
+    use numFalses = worker.Malloc(len)
+    use dStartTemp = worker.Malloc(len)
+    use dEndTemp = worker.Malloc(len)
+
+    // Number of iterations = bit count of the maximum number
+    let numIter = reducer.Reduce(dEnd.Ptr, len) |> getBitCount
+
+    let getArr i = if i &&& 1 = 0 then dStart, dEnd else dStartTemp, dEndTemp
+    let getOutArr i = getArr (i + 1)
+
+    for i = 0 to numIter - 1 do
+        // compute significant bits
+        let start, end' = getArr i
+        let outStart, outEnd = getOutArr i
+        worker.Launch <@ getNthSignificantReversedBit @> lp end'.Ptr i len dBits.Ptr
+
+        // scan the bits to compute starting positions further down
+        scanner.ExclusiveScan(dBits.Ptr, numFalses.Ptr, 0, len)
+
+        // scatter
+        worker.Launch <@ scatter @> lp start.Ptr end'.Ptr len numFalses.Ptr dBits.Ptr outStart.Ptr outEnd.Ptr
+    
+    let outStart, outEnd = getOutArr (numIter - 1)
+    outStart.Gather(), outEnd.Gather()        
