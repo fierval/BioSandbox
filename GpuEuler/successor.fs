01d717af59d1caa1282a112c3552f555bf1421dc
fierval <fierval@gmail.com>
2016-06-21 23:31:52 -0700

    groupSortedNums kernel

--- GpuEuler/successor.fs
+++ GpuEuler/successor.fs
@@ -11,6 +11,22 @@ open System.Collections.Generic
 
 let worker = Worker.Default
 
+[<Kernel;ReflectedDefinition>]
+let groupSortedNums (ends : deviceptr<int>) len (grouped : deviceptr<int>) =
+    let mutable idx = blockIdx.x * blockDim.x + threadIdx.x
+
+    if idx <= len then
+        if (idx <> 0 && idx < len && ends.[idx - 1] <> ends.[idx]) || idx = len then
+            let mutable n = 0
+            let cur = ends.[idx - 1]
+            idx <- idx - 1
+            while idx >= 0 && ends.[idx] = cur do
+                idx <- idx - 1
+                n <- n + 1
+            ends.[idx] <- n
+        else
+            ends.[idx] <- 0
+
 /// <summary>
 /// Same thing as graph.Reverse, however, since we know we are dealing with
 /// an Euler graph this function is much faster
