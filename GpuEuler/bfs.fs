6b1ac6d2f7ab286211b30c33bca3097edce7d5e7
fierval <fierval@gmail.com>
2016-07-25 01:13:00 -0700

    Reverting bfs.fs

--- GpuEuler/bfs.fs
+++ GpuEuler/bfs.fs
@@ -10,7 +10,8 @@ namespace GpuEuler
         open GpuCompact
 
         [<Kernel; ReflectedDefinition>]
-        let bfsKernel (front : deviceptr<bool>) len (frontOut : deviceptr<bool>) (visited : deviceptr<bool>) (level : deviceptr<int>) (edges : deviceptr<bool>) (rowIndex : deviceptr<int>) (colIndex : deviceptr<int>) (goOn : deviceptr<bool>) =
+        let bfsKernel (front : deviceptr<bool>) len (frontOut : deviceptr<bool>) (visited : deviceptr<bool>) (level : deviceptr<int>) (count : deviceptr<int>)
+            (edges : deviceptr<bool>) (rowIndex : deviceptr<int>) (colIndex : deviceptr<int>) (goOn : deviceptr<bool>) =
 
             let idx = blockIdx.x * blockDim.x + threadIdx.x
             if idx < len then
@@ -20,16 +21,30 @@ namespace GpuEuler
 
                     for i = rowIndex.[idx] to rowIndex.[idx + 1] - 1 do
                         let vertex = colIndex.[i]
-                        if not visited.[vertex] then
-                            let oldLevel = __atomic_exch (level + vertex) 1
-                            if oldLevel = 0 then
-                                level.[vertex] <- level.[idx] + 1
-                                frontOut.[vertex] <- true
+                        if not visited.[vertex] && level.[vertex] = 0 then
+                            level.[vertex] <- level.[idx] + 1
+                            __atomic_add (count + vertex) 1 |> ignore
+                            frontOut.[vertex] <- true
 
-                                edges.[i] <- true
-                                goOn.[0] <- true
+                            edges.[i] <- true
+                            goOn.[0] <- true
 
+        [<Kernel; ReflectedDefinition>]
+        let pruneKernel (rowIndex : deviceptr<int>) (colIndex : deviceptr<int>) len (edges : deviceptr<bool>) (count : deviceptr<int>) (level : deviceptr<int>) =
+            let idx = blockIdx.x * blockDim.x + threadIdx.x
+            if idx < len then
+                let mutable cnt = 0
 
+                let curLevel = level.[idx]
+
+                for i = rowIndex.[idx] to rowIndex.[idx + 1] - 1 do
+                    let vertex = colIndex.[i]
+                    // the current vertex has been visited from the vertex in question
+                    if count.[vertex] > 1 && level.[vertex] = curLevel + 1 then
+                        cnt <- cnt + 1
+                        // if the current vertex has been visited more than once - remove the edge
+                        if cnt > 1 then edges.[i] <- false
+            
         /// <summary>
         /// Generates spanning tree by bfs on the gpu
         //  In order to use weak connectivity, need to generate the undirected graph first
@@ -52,6 +67,7 @@ namespace GpuEuler
             use front = worker.Malloc(allFalse)
             use frontOut = worker.Malloc(allFalse)
             use dLevel = worker.Malloc(allZero)
+            use dCount = worker.Malloc(allZero)
 
             front.ScatterScalar true
 
@@ -62,7 +78,8 @@ namespace GpuEuler
             while goOn.GatherScalar() do
                 goOn.ScatterScalar(false)
 
-                worker.Launch <@ bfsKernel @> lp (getFront flag).Ptr len (getFront (not flag)).Ptr dVisted.Ptr dLevel.Ptr dEdges.Ptr dRowIndex.Ptr dColIndex.Ptr goOn.Ptr
+                worker.Launch <@ bfsKernel @> lp (getFront flag).Ptr len (getFront (not flag)).Ptr dVisted.Ptr dLevel.Ptr dCount.Ptr dEdges.Ptr dRowIndex.Ptr dColIndex.Ptr goOn.Ptr
                 flag <- not flag
 
-            dEdges.Gather()
+            worker.Launch <@ pruneKernel @> lp dRowIndex.Ptr dColIndex.Ptr len dEdges.Ptr dCount.Ptr dLevel.Ptr
+            dEdges.Gather(), dCount.Gather(), dLevel.Gather()
