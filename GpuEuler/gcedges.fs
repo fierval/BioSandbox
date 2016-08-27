f81edd8304a8ed6e76b27f370302b758576cc775
fierval <fierval@gmail.com>
2016-08-26 19:19:39 -0700

    GPU version of GC graph generation

--- GpuEuler/gcedges.fs
+++ GpuEuler/gcedges.fs
@@ -9,12 +9,15 @@ namespace GpuEuler
         open System.Collections.Generic
         open GpuCompact
 
+        [<Literal>]
+        let MaxColros = 1000
+
         /// <summary>
-        /// This takes the REVERSE graph rowIndex and generates an array of valid swaps
+        /// Takes the graph rowIndex and generates an array of valid swaps
         /// </summary>
         /// <param name="rowIndex">rowIndex of a reverse eulerian graph</param>
         /// <param name="colors">partition created in the previous step</param>
-        let generateCircuitGraph (rowIndex : int []) (colors : int []) =
+        let generateCircuitGraphLinear (rowIndex : int []) (colors : int []) =
             let numColors = 1 + (colors |> Array.max)
             let numVertices = rowIndex.Length - 1
             let numEdges = rowIndex.[rowIndex.Length - 1]
@@ -51,6 +54,70 @@ namespace GpuEuler
             eb.AddRange eaArr
             links.AddRange links
 
+            ea.ToArray(), eb.ToArray(), links.ToArray(), validity
+
+        [<Kernel; ReflectedDefinition>]
+        let circuitGraphKernel (rowIndex : deviceptr<int>) numVertices (links : deviceptr<int>) (validity : deviceptr<int>) (ea : deviceptr<int>) (eb : deviceptr<int>) (colors : deviceptr<int>) =
+
+            let idx = blockDim.x * blockIdx.x + threadIdx.x
+            if idx < numVertices then
+                let status : bool [] = __local__.Array(MaxColros)
+                for i = 0 to status.Length do
+                    status.[i] <- false
+
+                let mutable pre = 0
+
+                for j = rowIndex.[idx] to rowIndex.[idx + 1] - 1 do
+                    if not status.[colors.[j]] then
+                        status.[colors.[j]] <- true
+                    else
+                        validity.[j] <- 0
+
+                pre <- rowIndex.[idx]
+                for j = rowIndex.[idx] + 1 to rowIndex.[idx + 1] - 1 do
+                    if validity.[j] = 1 then
+                        ea.[j] <- colors.[j]
+                        eb.[j] <- colors.[pre]
+                        links.[j] <- pre
+                        pre <- j
+
+        let generateCircuitGraphGpu (rowIndex : int []) (colors : int []) =
+            let numVertices = rowIndex.Length - 1
+            let lp = LaunchParam(divup numVertices blockSize, blockSize)
+            let numEdges = rowIndex.[rowIndex.Length - 1]
+
+            let zeros : int [] = Array.zeroCreate numEdges
+            let ones : int [] = Array.create numEdges 1
+
+            use dLinks = worker.Malloc<int>(zeros)
+            use dValidity = worker.Malloc<int>(ones)
+            use dEa = worker.Malloc<int>(zeros)
+            use dEb = worker.Malloc<int>(zeros)
+            use dColors = worker.Malloc(colors)
+            use dRowIndex = worker.Malloc(rowIndex)
+
+            worker.Launch <@circuitGraphKernel@> lp dRowIndex.Ptr numVertices dLinks.Ptr dValidity.Ptr dEa.Ptr dEb.Ptr dColors.Ptr
+
+            let compact (a : DeviceMemory<int>) = a |> compactGpu |> (fun c -> c.Gather())
+
+            let ea = compact dEa
+            let eb = compact dEb
+            let links = compact dLinks
+            let validityInt = compact dValidity
+
+            ea, eb, links, (validityInt |> Array.map (fun e -> e = 1))
+
+        /// <summary>
+        /// Takes the graph rowIndex and generates an array of valid swaps
+        /// </summary>
+        /// <param name="rowIndex">rowIndex of a reverse eulerian graph</param>
+        /// <param name="colors">partition created in the previous step</param>
+        let generateCircuitGraph (rowIndex : int[]) (colors : int []) =
+            let ea, eb, links, validity =
+                if colors.Length < 1000 then
+                    generateCircuitGraphGpu rowIndex colors
+                else
+                    generateCircuitGraphLinear rowIndex colors
 
             let starts, colIndex, linkPos =
                 Seq.zip3 ea eb links
