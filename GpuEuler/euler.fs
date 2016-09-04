89050c600548aa4ac2a98b4b043020a6945eedda
fierval <fierval@gmail.com>
2016-09-04 13:49:44 -0700

    CG CPU generation warning

--- GpuEuler/euler.fs
+++ GpuEuler/euler.fs
@@ -15,7 +15,7 @@ module Euler =
         // Create a line graph from the successor array:
         let partition, maxPartition = partitionLinear edgePredecessors
         if maxPartition = 1 then printfn "Generated Euler cycle in 2 passes"
-
+        if maxPartition > 1000 then printfn "CG CPU generation"
         if maxPartition <> 1 then
             // 3. Create GC graph, where each vertex is a partition of the
             // Successor linear graph
