745e08a0a3314d35188e30bdce97043592c94c9b
fierval <fierval@gmail.com>
2017-05-23 09:25:30 -0700

    Adding instrumentation

--- Alea3Scan/Program.fs
+++ Alea3Scan/Program.fs
@@ -2,23 +2,17 @@
 open Alea.Parallel
 open System
 
-[<EntryPoint>]
+[<EntryPoint; GpuManaged>]
 let main argv = 
     
     let len = 100 * 100
     let arr = [|0..len - 1|]
-    let mutable arrOut = Array.zeroCreate len
+    let arrOut = Array.zeroCreate len
     let gpu = Gpu.Default;
     use session = new Session(gpu)
-    use devIn = gpu.AllocateDevice(arr)
-    use devOut = gpu.AllocateDevice(arrOut)
-    
-    session.Scan(devOut.Ptr, devIn.Ptr, 0, len, (fun x y -> x + y), 0)
-    arrOut <- Gpu.CopyToHost(devOut)
+
+    session.Scan(arrOut, arr, 0, (fun x y -> x + y), 0)
     printfn "%A" arrOut
-    
-    let arrScan = arr |> Array.scan(fun st y -> st + y) 0
-    printfn "%b" (arrOut = arrScan.[1..])
 
     0 // return an integer exit code
 
